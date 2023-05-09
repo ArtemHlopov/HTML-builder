1abb0341c0ccb4444372c96038cf5763a21cfb04
Artem <artemhlopov92@gmail.com>
2023-05-09 15:11:46 +0300

    add solution for task 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const curPath = path.join(__dirname, "styles");
+const newPath = path.join(__dirname, "project-dist", "bundle.css");
+
+async function bundle() {
+  const files = await fs.readdir(curPath, { withFileTypes: true });
+
+  let bundle = "";
+  for (let file of files) {
+    if (path.extname(file.name) === ".css") {
+      let filePath = path.join(curPath, file.name);
+      let data = await fs.readFile(filePath, "utf-8");
+      bundle += data;
+    }
+  }
+  await fs.writeFile(newPath, bundle);
+}
+bundle();
