bcf3d7ef535db38b0a705df01c7af5d2825fd793
Artem <artemhlopov92@gmail.com>
2023-05-10 19:52:16 +0300

    fix: fix solution for task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,20 +4,28 @@ const path = require("path");
 const dirCur = path.join(__dirname, "files");
 const dirCopy = path.join(__dirname, "files-copy");
 
-async function copy() {
+async function copy(src, dist) {
   try {
-    await fs.access(dirCopy);
+    await fs.access(destDir);
   } catch (error) {
     await fs.mkdir(dirCopy, { recursive: true });
   }
 
-  const data = await fs.readdir(dirCur, { withFileTypes: true });
-
-  for (let file of data) {
-    await fs.copyFile(
-      path.join(dirCur, file.name),
-      path.join(dirCopy, file.name)
-    );
+  const data = await fs.readdir(src, { withFileTypes: true });
+  for (const file of data) {
+    if (file.isDirectory()) {
+      await copy(path.join(src, file.name), path.join(dist, file.name));
+    } else {
+      await fs.copyFile(path.join(src, file.name), path.join(dist, file.name));
+    }
   }
 }
-copy();
+async function clearDir() {
+  try {
+    await fs.rm(dirCopy, { recursive: true });
+  } catch (error) {
+    return;
+  }
+}
+clearDir();
+copy(dirCur, dirCopy);
