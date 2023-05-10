aea3115fbf5af2a3f5ffa736a85e9505d4e1df4c
Artem <artemhlopov92@gmail.com>
2023-05-10 18:47:11 +0300

    fix: fixed outpit file name

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,7 +9,7 @@ fs.readdir(folderPath, { withFileTypes: true }, (error, data) => {
   data.forEach((el) => {
     if (el.isFile()) {
       console.log(
-        `${el.name.split(".")[1]} - ${path.extname(el.name)} - ${
+        `${el.name.split(".")[0]} - ${path.extname(el.name).slice(1)} - ${
           fs.statSync(`${folderPath}/${el.name}`).size / 1000
         } kb`
       );
